faead84b1fdfbcb1882a906f86770b888a2b560c
Demid-Grishanovich <demidgrishs@gmail.com>
2023-11-19 16:52:13 +0200

    Change News

--- src/main/java/com/newsportal/model/News.java
+++ src/main/java/com/newsportal/model/News.java
@@ -1,6 +1,7 @@
-package com.newsportal.model;
+package com.newsportal.model; // Измените на ваш пакет
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "news")
@@ -8,13 +9,56 @@ public class News {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id")
     private Long id;
 
-    @Column(name = "title")
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "title", nullable = false, length = 255)
     private String title;
 
-    @Column(name = "content")
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
+
+    @Column(name = "image_url", length = 255)
+    private String imageUrl;
+
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @Column(name = "is_main_news")
+    private Boolean isMainNews = false;
+
+    @Column(name = "priority")
+    private Integer priority = 0;
+
+    @Column(name = "views")
+    private Integer views = 0;
+
+    @Column(name = "likes")
+    private Integer likes = 0;
+
+    @Column(name = "dislikes")
+    private Integer dislikes = 0;
+
+    @Column(name = "status_id", length = 50)
+    private String statusId;
+
+    @Column(name = "public_at")
+    private LocalDateTime publicAt;
+
+    @Column(name = "unpublic_at")
+    private LocalDateTime unpublicAt;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
 
     public Long getId() {
         return id;
@@ -22,6 +66,14 @@ public class News {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public String getTitle() {
@@ -40,7 +92,107 @@ public class News {
         this.content = content;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 
+    public Integer getCategoryId() {
+        return categoryId;
+    }
 
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Boolean getMainNews() {
+        return isMainNews;
+    }
+
+    public void setMainNews(Boolean mainNews) {
+        isMainNews = mainNews;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Integer getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(Integer dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public String getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(String statusId) {
+        this.statusId = statusId;
+    }
+
+    public LocalDateTime getPublicAt() {
+        return publicAt;
+    }
+
+    public void setPublicAt(LocalDateTime publicAt) {
+        this.publicAt = publicAt;
+    }
+
+    public LocalDateTime getUnpublicAt() {
+        return unpublicAt;
+    }
+
+    public void setUnpublicAt(LocalDateTime unpublicAt) {
+        this.unpublicAt = unpublicAt;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
