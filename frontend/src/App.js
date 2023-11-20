341fc670f2b681d9efb72ab5ddd32daa166ac724
Yauheniya <ya.kavalchuk@gmail.com>
2023-11-20 20:29:40 +0300

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
-import HomePage from './pages/HomePage';
-import CategoriesPage from './pages/CategoriesPage';
-import { BrowserRouter as Router } from 'react-router-dom';
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import CategoriesPage from "./pages/CategoriesPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+        </Routes>
+      </Router>
     </div>
-    </Router>
   );
 }
 
