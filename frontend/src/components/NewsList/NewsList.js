eda95a008639d3bf0d97dddd35513a60601b8aa8
Yauheniya <ya.kavalchuk@gmail.com>
2023-11-23 17:53:07 +0300

    make newslist flex

--- frontend/src/components/NewsList/NewsList.js
+++ frontend/src/components/NewsList/NewsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Tag } from 'antd';
+import { Flex, Tag } from 'antd';
 import NewsCard from '../NewsCard/NewsCard';
 import './NewsList.css';
 
@@ -10,13 +10,13 @@ const NewsList = ({ category, newsList }) => {
       <h2 className="news-list-title">
         {category && <Tag color="blue">{category.name}</Tag>}
       </h2>
-      <Row className="news-row" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+      <Flex className="news-row" justify='space-between'>
         {newsList.map((news) => (
-          <Col key={news.id} xs={24} sm={12} md={6} span={8}>
+          <div key={news.id} xs={24} sm={12} md={6} span={8}>
             <NewsCard news={news} />
-          </Col>
+          </div>
         ))}
-      </Row>
+      </Flex>
     </div>
   );
 };
