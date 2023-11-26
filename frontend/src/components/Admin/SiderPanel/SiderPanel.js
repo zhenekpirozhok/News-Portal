d8e24deffa7addcb027577c749559e05b10a4761
Yauheniya <ya.kavalchuk@gmail.com>
2023-11-26 22:33:28 +0300

    add sider panel

---
+++ frontend/src/components/Admin/SiderPanel/SiderPanel.js
@@ -0,0 +1,34 @@
+// SiderPanel.js
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { UserOutlined, FileOutlined } from '@ant-design/icons';
+import './SiderPanel.css'; // Add your CSS file if needed
+
+const { Sider } = Layout;
+
+const SiderPanel = () => {
+  return (
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={(broken) => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="demo-logo-vertical" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1" icon={<FileOutlined />}>
+          News
+        </Menu.Item>
+        <Menu.Item key="2" icon={<UserOutlined />}>
+          Users
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
+};
+
+export default SiderPanel;
--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Layout, Menu, Flex } from "antd";
 import { UserOutlined, DiffOutlined } from "@ant-design/icons";
-import SidePanel from "../components/Admin/SidePanel/SidePanel";
+import SidePanel from "../components/Admin/SiderPanel/SiderPanel";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -18,20 +18,6 @@ const AdminPage = ({ children }) => {
       <Layout>
         <Header style={{ padding: 0, background: "#001529" }} />
         <Content style={{ margin: "24px 16px 0" }}>
-          <div style={{ padding: 24, minHeight: 360, background: "#fff" }}>
-            <Menu
-              mode="horizontal"
-              selectedKeys={[selectedTab]}
-              onClick={({ key }) => handleMenuClick(key)}
-            >
-              <Menu.Item key="users" icon={<UserOutlined />}>
-                Users
-              </Menu.Item>
-              <Menu.Item key="news" icon={<DiffOutlined />}>
-                News
-              </Menu.Item>
-            </Menu>
-
             {selectedTab === "users" && (
               <div>
                 {/* Render content for the "Users" tab */}
@@ -47,7 +33,6 @@ const AdminPage = ({ children }) => {
                 {children}
               </div>
             )}
-          </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
           ©2023 EPAM ESDE News Portal
