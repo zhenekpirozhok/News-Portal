1d6fceabdeee1654c09da29b47cf12d9b6e92670
Hanna Drozhdzh <hanna.drozhdzh@gmail.com>
2023-11-28 23:32:00 +0200

    change authorName logo

--- frontend/src/components/AuthorName/AuthorName.js
+++ frontend/src/components/AuthorName/AuthorName.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 import './AuthorName.css'; 
 
-const AuthorName = ({ authorName, avatarUrl }) => {
+const AuthorName = ({ authorName, avatarUrl, gap }) => {
   return (
     <div className="author-container">
-      <Avatar src={avatarUrl} size={64} />
+      <Avatar size={60} icon={<UserOutlined />}>
+      </Avatar>
       <div className="author-info">
         <span className="author-name">{authorName}</span>
       </div>
